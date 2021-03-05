c19f63d856b579539105a7a827bdefa0d0d2cf35
starcevic-ivan <starcevic-ivan@outlook.com>
2021-03-05 08:44:10 +0100

    inital commit

---
+++ app/src/main/java/ch/starcevic/zli/m335/partytogo_ballongame/MainActivity.java
@@ -0,0 +1,14 @@
+package ch.starcevic.zli.m335.partytogo_ballongame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
